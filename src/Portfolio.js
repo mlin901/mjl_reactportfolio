9414662e3479acbab0915c60fc08b28ef8e1e746
Matt Linder <mjlinder218@gmail.com>
2021-08-20 19:26:48 -0700

    Initial work needed to implement project cards and their styling in Portfolio component

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import ProjectList from "./ProjectList";
+import marvelizer from './images/Marvelizer2.jpg';
+
 
 const Portfolio = () => {
   const [projects, setProjects] = useState([
-    { id: 1, title: 'A', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'A summary', description: 'A description' },
-    { id: 2, title: 'B', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'B summary', description: 'B description' },
-    { id: 3, title: 'C', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'C summary', description: 'C description' },
-    { id: 4, title: 'D', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'D summary', description: 'D description' },
-    { id: 5, title: 'E', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'E summary', description: 'E description' },
-    { id: 6, title: 'F', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'F summary', description: 'F description' },
+    { id: 0, title: 'The MARVELizer', image: marvelizer, deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'A summary', description: ' is a web app that enables you to search for a Marvel character, view information and Giphys related to the character, and save favorite Giphys. The search feature queries the Marvel API for a character’s description and a thumbnail, and it uses the character name to query the Giphy API for the Giphys related to the character. <br /> This application utilizes <b>Foundation</b>, <b>APIs</b> (Marvel and Giphy), <b>JQuery</b>, <b>JavaScript</b>, and <b>local storage</b>.<br /> <br /><a className="proj_links" href="https://github.com/mlin901/Marvelizer"> ➤&nbsp;&nbsp;GitHub repo</a><br /><a class="proj_links" href="https://zoneam.github.io/Marvelizer/"> ➤&nbsp;&nbsp;Published site</a>' },
+    { id: 1, title: 'B', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'B summary', description: 'B description' },
+    { id: 2, title: 'C', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'C summary', description: 'C description' },
+    { id: 3, title: 'D', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'D summary', description: 'D description' },
+    { id: 4, title: 'E', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'E summary', description: 'E description' },
+    { id: 5, title: 'F', image: 'none', deployed: 'https://wwww.google.com', github: 'https://wwww.google.com', summary: 'F summary', description: 'F description' },
   ]);
 
   return ( 
@@ -18,6 +20,13 @@ const Portfolio = () => {
           <div class="cardcontainer">
             <figure class="cardfig">
               <div class="img__wrap">
+                <img src={ projects[0].image } alt="The Marvelizer web app"></img>
+                <div class="img__description_layer">
+                  <p class="img__description"> <span class="proj_title">{ projects[0].title }</span>
+                    { projects[0].description }
+                  </p>
+                </div>
+
 
               </div>
             </figure>
